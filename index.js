c856614878a33ca88e642c8054d6475c0c80d18a
Mishuzo XzyyMDS <xzyytr@gmail.com>
2026-01-30 14:18:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
