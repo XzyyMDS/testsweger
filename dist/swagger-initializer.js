8fd31b7fe38610d90b3782ac58e66560752eb07d
Mishuzo XzyyMDS <xzyytr@gmail.com>
2026-01-30 17:32:08 +0800

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,21 +1,17 @@
 window.onload = function() {
-
-  //<editor-fold desc="Changeable Configuration Block">
-
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
  fetch('swagger-ui.json')
   .then(response => response.json())
   .then(spec => {
     const ui = SwaggerUIBundle({
       spec: spec,
       dom_id: '#swagger-ui',
-      presets: [
-        SwaggerUIBundle.presets.apis,
-        SwaggerUIBundle.SwaggerUIStandalonePreset
-      ],
-      layout: "BaseLayout"
-    });
+        presets: [
+    SwaggerUIBundle.presets.apis,
+    SwaggerUIBundle.SwaggerUIStandalonePreset
+  ],
+  layout: "StandaloneLayout",
+  displayRequestDuration: true,
+  docExpansion: "none"
+      });
   });
-
-  //</editor-fold>
 };
