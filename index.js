0eee33ef11b5c747126720416a62ca046b22a3dc
Mishuzo XzyyMDS <xzyytr@gmail.com>
2026-01-30 14:13:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-
 const app = express();
+const path = require('path');
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
